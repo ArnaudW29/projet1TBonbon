84ecfe4fd9e31f1270430228f99de02ddbe2b5da
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-08-07 01:54:19 +0200

    Update insert-into.sql

--- BACKEND/script-BDD/insert-into.sql
+++ BACKEND/script-BDD/insert-into.sql
@@ -26,14 +26,15 @@ VALUES
 ('cl02','waetermans','arnaud','rue de virginal 21 7090 BELGIQUE','admin','admin29','awj29@outlook.com'),
 ('cl03','salamendro','luis','avenue saint huber 1 1480 BELGIQUE','luisendro','salam1480','luis.salamendro@yahoo.com');
 
-/* donnée des ventes de chaque produits */
-
-INSERT tbVentes(commandesId,bonbonId,ventesNum,ventesQte)
-VALUES
-('co01','b01',1,5);
 
 /* donnée des commandes des clients (test)  */
 
 INSERT INTO tbCommandes(commandesId,clientId,commandesPrixTOT,commandesQteTOT)
 VALUES
 ('co01','cl01',5.45,5);          // 5*b01
+
+/* donnée des ventes de chaque produits */
+
+INSERT tbVentes(commandesId,bonbonId,ventesNum,ventesQte)
+VALUES
+('co01','b01',1,5);
