90680dfb2146a839e25da0373a7b023311bb7c5b
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-08-07 00:58:02 +0200

    Update creation-tables.sql

--- BACKEND/script-BDD/creation-tables.sql
+++ BACKEND/script-BDD/creation-tables.sql
@@ -1,1 +1,61 @@
-
+/* Table contenant les infromations des marques de bonbons */ 
+CREATE TABLE tbMarque(
+  marqueId CHAR(3) NOT NULL, /* m01,m02,m03,m04 */
+  marqueNom VARCHAR(30) NOT NULL, /* lutti, haribo etc.. */
+  CONSTRAINT pk_tbMarque PRIMARY KEY (marqueId)
+  );
+     /* Table contenant les informations du client */
+   CREATE TABLE tbClient(
+     clientId CHAR(4) NOT NULL, /* cl01,cl02, cl03 , cl04 */
+     clientNom VARCHAR(30) NOT NULL, /* DUPONT , DUBOIS */
+     clientPrenom VARCHAR(30) NOT NULL, /* DENIS, LOIC */
+     clientAdresse VARCHAR(80) NOT NULL, /* rue du panier 24 1460 belgique */
+     clientIdentifiant VARCHAR(50) NOT NULL, /* admin */
+     clientMdp VARCHAR(20) NOT NULL, /* admin1234 */
+     clientMail VARCHAR(70) NOT NULL,  /* DUPONT.loic@hotmail.com */
+     CONSTRAINT pk_Client PRIMARY KEY (clientId)
+     );
+      /* Table contenant les inforamations des bonbons*/
+     CREATE TABLE tbBonbon(
+       bonbonId CHAR(3) NOT NULL, /* b01,b02,b03,b04 */
+       bonbonNom VARCHAR(45) NOT NULL, /* sucette a la pomme , sucette a la poire etc */
+       bonbonGout VARCHAR(15) NOT NULL,
+       bonbonType VARCHAR(15) NOT NULL, /* sucrés ,acidulés */
+       bonbonPrix DECIMAL(4,2) NOT NULL,
+       bonbonQte VARCHAR(15) NOT NULL, 
+       marqueId CHAR(3) NOT NULL,
+       CONSTRAINT pk_tbBonbon PRIMARY KEY (bonbonId),
+       CONSTRAINT fk_tbBonbon_tbMarque FOREIGN KEY (marqueId) REFERENCES tbMarque ( marqueId)
+       on UPDATE CASCADE ON DELETE RESTRICT
+       );
+     /* Table contenant l'historique des commandes de l'utilisateur */
+    CREATE TABLE tbCommandes(
+       commandesId CHAR(4) NOT NULL , /* co01, co02, co03, co04 */
+       clientId CHAR(4) NOT NULL,  /* cl01,cl02, cl03 , cl04 */
+       commandesPrixTOT DECIMAL(6,2) NOT NULL,
+       commandesQteTOT INTEGER NOT NULL,
+       commandesDate datetime NOT NULL DEFAULT getDate(),
+       CONSTRAINT pk_tbCommandes PRIMARY KEY (commandesId),
+       CONSTRAINT fk_tbCommandes_tbClient FOREIGN KEY (clientId) REFERENCES tbClient (clientId)
+       ON UPDATE CASCADE ON DELETE RESTRICT
+       );
+     /* Table contenant les informations du panier de l'utilisateur */
+   CREATE TABLE tbPanier(
+     clientId CHAR(4) NOT NULL,     /* cl01, cl02, cl03, cl04 */
+     bonbonId CHAR(3) NOT NULL,     /* b01,b02,b03,b04 */
+     panierQte INTEGER NOT NULL,
+     CONSTRAINT fk_tbPanier_tbBonbon FOREIGN KEY (bonbonId) REFERENCES tbBonbon (bonbonId),
+     CONSTRAINT fk_tbPanier_tbClient FOREIGN KEY (clientId) REFERENCES tbClient (clientId)
+     );
+      /* Table contenant les informations de la commande de l'utilisateur */
+    CREATE TABLE tbVentes(
+      commandesId CHAR(4) NOT NULL, /* co01, co02, co03, co04 */
+      bonbonId CHAR(3) NOT NULL, /* b01,b02,b03,b04 */
+      ventesNum INTEGER NOT NULL,
+      ventesQte INTEGER NOT NULL,
+      CONSTRAINT fk_tbVentes_tbBonbon FOREIGN KEY (bonbonId) REFERENCES tbBonbon (bonbonId) 
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+      CONSTRAINT fk_tbVentes_tbCommandes FOREIGN KEY (commandesId) REFERENCES tbCommandes (commandesId)
+      ON UPDATE CASCADE ON DELETE RESTRICT
+      );
+ 
