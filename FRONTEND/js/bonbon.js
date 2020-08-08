34e5cad8408a6037a0d0ced4b55df1a6367b2c80
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-08-08 15:52:30 +0200

    Add files via upload

---
+++ FRONTEND/js/bonbon.js
@@ -0,0 +1,60 @@
+function gid(id){
+    return document.getElementById(id);
+}
+function initBonbon() { // Initialise la page avec le catalogue de bonbon
+    let xhr = new XMLHttpRequest();
+    xhr.open('get', "http://localhost:82/initBonbon", true);
+    xhr.onload =
+        function manageReponse() {
+            let rep = JSON.parse(xhr.responseText);
+            let unBonbon= "";
+            let c= "";
+            for(let i of rep){
+                c++
+                console.log(c)
+                if(c > 4)
+                {
+
+                    c = 1;
+                }
+                unBonbon +=
+                    "<div class='sectionUnBonbon" + c+"'>" +
+                    "<div class='photoBonbon'>" +
+                    "<img id='photo" + i.bonbonId +"' src='img/" + i.bonbonId + ".png' alt='"+ i.bonbonNom +"'>" +
+                    "</div>" +
+                    "<div class='info'>" + "" +
+                    "<span class='nomBonbon'>" + i.bonbonNom + "</span>"+" " +
+                    "<span class='marqueBonbon'>"+ i.marqueNom+"</span><br>" +
+                    "<span class='typeBonbon'>"+ i.bonbonType+"</span><br>" +
+                    "<span class='QteBonbon'>"+ i.bonbonQte +"</span><br>" +
+                    "<span class='prixBonbon'>"+ i.bonbonPrix + "€</span></div>" +
+                    "<div id='formAjout"+i.bonbonId+"'>" +
+                    "<form action='#' onsubmit='ajoutBonbon("+'"'+i.biere+'","'+i.id+'","'+i.brasserie+'",'+i.volume+',this.qtt.value'+','+i.prix+"); return false;'>" +
+                    "<input id='input"+i.bonbonId+"'  class='cbBonbon' name='qtt' type='number' min='1' value='0' step='1'>" +
+                    "<input type='submit' value='Ajouter' class='ajoutBonbon'></form></div></div>";
+            }
+
+            gid("bonbon").innerHTML = unBonbon;
+        };
+    xhr.send();
+}
+function ajoutBonbon(biere,id,brasserie,volume,qtt,prix){
+    if(clId===""){
+        alert("Veuillez vous connecter avant de commander");
+        return false;
+    }
+    let jsonFeedBack = {};
+    let feedback ="";
+    let idFeedBack="";
+    let url='insertPanier?bId='+id+'&qtt='+qtt+'&cId='+clId;
+    let xhr = new XMLHttpRequest();
+    xhr.open('get',url);
+    xhr.onload=function(){
+        jsonFeedBack= Object.assign(jsonFeedBack,JSON.parse(xhr.responseText));
+        idFeedBack="inputNombre"+id;
+        document.getElementById(idFeedBack).value = jsonFeedBack[0].quant;
+    };
+    xhr.onerror=function(){console.log("xhr error");}
+    xhr.send();
+
+}
--- FRONTEND/js/connexion.js
+++ FRONTEND/js/connexion.js
@@ -1,6 +1,6 @@
 function connexion() {
     document.getElementById("connexion").innerHTML = "<div id=\"connexionH\">\n" +
-        "    <form id=\"formulaire_connexion\" action=# onsubmit=\"connexionCommande(this); return false;\">\n" +
+        "    <form id=\"formulaire_connexion\" action=# onsubmit=\"connexionCommande(); return false;\">\n" +
         "<!-- username du client -->\n" +
         "<label for=\"indentifiant\">Identifiant : </label>\n" +
         "<input id=\"identifiant\" name=\"identifiant\" type=\"text\" required placeholder=\"Admin\"><br>\n" +
@@ -11,4 +11,27 @@ function connexion() {
         "<input id=\"boutonConnexion\" class=\"boutonConnexion\" type=\"submit\" value=\"Connexion\">\n" +
         "    </form>\n" +
         "    </div>";
-};
+}
+function connexionCommande(){
+    let clientClient={};
+    let xhr =  new XMLHttpRequest();
+    xhr.open('get','getconnexion?id='+gid('identifiant').value+'&mdp='+gid('mdp').value,false);
+    xhr.onload = function (){
+        clientClient = JSON.parse(xhr.responseText);
+        if(Object.keys(clientClient).length === 0){
+            document.getElementById("clientConnected").innerHTML="<p class='error'> Mauvais identifiant ou mauvais mot de passe</p>";
+        }
+        else{
+            console.log(clientClient)
+            document.getElementById("connexion").innerHTML="";
+            document.getElementById("clientConnected").innerHTML="Actuellement connecté(e): " +"<strong>"+(clientClient[0].clientPrenom).toUpperCase()+"</strong>";
+            document.getElementById("clientConnected").innerHTML+="<br><button id='boutonDeconnexion' class='boutonDeconnexion' onclick='deco();'>Deconnexion</button>";
+        }
+    };
+    xhr.send();
+
+}
+function deco(){
+    document.getElementById("clientConnected").innerHTML="";
+    connexion();
+}
