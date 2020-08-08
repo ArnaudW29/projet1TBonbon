431504bb4d9b723d808a901fec1bf4fc89b39d80
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-08-08 16:02:38 +0200

    Update and rename webservice.sql to insertConnexion.sql

---
+++ BACKEND/script-api-rest/insertConnexion.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE "dba"."proc_inserConnexion"(in clientId char(4), in clientMdp varchar(20), in clientIdentifiant VARCHAR(50))
+BEGIN 
+    INSERT INTO tbConnexion(clientId, clientMdp, clientIdentifiant)
+VALUES
+(clientId,ClientMdp,ClientIdentifiant)
+END;
+
+
+--------------------------/* service d'insertion dans la table connexion */-----------------------------
+
+CREATE SERVICE "proc_insertConnexion" TYPE 'RAW' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.proc_insertConnexion(:clientId,:clientMdp,clientIdentifiant);
