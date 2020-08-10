980024c1aab889ede82e4aed5ba0b85479b38df7
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-08-10 18:01:11 +0200

    Create proc_suppressionPanierComplet.sql

---
+++ BACKEND/script-api-rest/proc_suppressionPanierComplet.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE "dba"."proc_suppressionPanierComplet"(IN clId char(4))
+BEGIN 
+    DELETE FROM tbPanier
+    WHERE clId= clientId
+END;
+------------------------------/* service de suppression de toutes la table panier en fonctione du clientId */-----------------
+
+CREATE SERVICE "proc_suppressionPanierComplet" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call DBA.proc_suppressionPanierComplet(:clId);
