d0fba36571baf208b90250158ad09f655077436e
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-08-10 19:34:54 +0200

    Update insertInscription.sql

--- BACKEND/script-api-rest/insertInscription.sql
+++ BACKEND/script-api-rest/insertInscription.sql
@@ -6,4 +6,4 @@ VALUES
 END;
 
 ----------------------------/* service pour inserer les inscriptions dans la table client */-----------------------------------
-CREATE SERVICE "insertIncsription" TYPE 'RAW' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.insertInscription(:Nom,:Prenom,Adresse,Identifiant,Mdp,Mail);
+CREATE SERVICE "insertInscription" TYPE 'RAW' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.insertInscription(:Nom,:Prenom,Adresse,Identifiant,Mdp,Mail);
