794a11053bd9a34e602c18d55320fb7f4613bb96
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-08-10 18:37:14 +0200

    Update insertInscription.sql

--- BACKEND/script-api-rest/insertInscription.sql
+++ BACKEND/script-api-rest/insertInscription.sql
@@ -1,9 +1,9 @@
-CREATE PROCEDURE "dba"."insertInscription"(in Id char(4), in Nom varchar(30), in Prenom VARCHAR(30), in Adresse varchar(80), in Identifiant varchar(50), in Mdp varchar(20), in Mail varchar(70))
+CREATE PROCEDURE "dba"."insertInscription"(in Nom varchar(30), in Prenom VARCHAR(30), in Adresse varchar(80), in Identifiant varchar(50), in Mdp varchar(20), in Mail varchar(70))
 BEGIN
-INSERT INTO tbClient(clientId, clientNom, clientPrenom, clientAdresse, clientIdentifiant,clientMdp,clientMail)
+INSERT INTO tbClient(clientNom, clientPrenom, clientAdresse, clientIdentifiant,clientMdp,clientMail)
 VALUES 
-(Id,Nom,Prenom,Adresse,Identifiant,Mdp,Mail)
+(Nom,Prenom,Adresse,Identifiant,Mdp,Mail)
 END;
 
 ----------------------------/* service pour inserer les inscriptions dans la table client */-----------------------------------
-CREATE SERVICE "insertIncsription" TYPE 'RAW' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.insertInscription(:Id,:Nom,:Prenom,Adresse,Identifiant,Mdp,Mail);
+CREATE SERVICE "insertIncsription" TYPE 'RAW' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.insertInscription(:Nom,:Prenom,Adresse,Identifiant,Mdp,Mail);
