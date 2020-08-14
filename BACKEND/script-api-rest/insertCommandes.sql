e310917e684e9a9a0545647021f85c744246d787
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-08-14 23:43:31 +0200

    Update insertCommandes.sql

--- BACKEND/script-api-rest/insertCommandes.sql
+++ BACKEND/script-api-rest/insertCommandes.sql
@@ -1,9 +1,9 @@
-CREATE PROCEDURE "DBA"."proc_insertCommandes"(in commandesId INTEGER, in clientId INTEGER, in commandesPrixTOT DECIMAL(6,2), in commandesQteTOT INTEGER)
+CREATE PROCEDURE "DBA"."proc_insertCommandes"(in commId INTEGER, in clId INTEGER, in commPrixTOT DECIMAL(6,2), in commQteTOT INTEGER)
 BEGIN
     INSERT INTO tbCommandes(commandesId,clientId,commandesPrixTOT,commandesQteTOT)
-        VALUES (commandesId,clientId,commandesPrixTOT,commandesQteTOT)
+        VALUES (commId,clId,commPrixTOT,commQteTOT)
 END;
 
 
 ------------------------/* service pour inserer la commandes  */-----------------------------------
-CREATE SERVICE "proc_insertCommandes" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS CALL dba.proc_insertCommandes(:commandesId,:clientId,:commandesPrixTOT,:commandesQteTOT);
+CREATE SERVICE "proc_insertCommandes" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS CALL dba.proc_insertCommandes(:commId,:clId,:commPrixTOT,:commQteTOT);
