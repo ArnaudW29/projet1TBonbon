411721532b917da1ff2dea3b845d7e008c6681c7
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-08-15 01:10:38 +0200

    Update and rename procedure-getHistorique.sql to procedure-getCommandes.sql

--- BACKEND/scripts-web-service/procedure-getCommandes.sql
+++ BACKEND/scripts-web-service/procedure-getCommandes.sql
@@ -1,13 +1,13 @@
 
-CREATE PROCEDURE "DBA"."proc_getHistorique"(IN commId INTEGER)
+CREATE PROCEDURE "DBA"."proc_getCommandes"(IN commId INTEGER)
 RESULT(commandesId INTEGER,clientId INTEGER, commandesPrixTOT decimal(6,2), commandesQteTOT integer, commandesDate datetime)
 BEGIN
     call sa_set_http_header('Access-Control-Allow-Origin','*');
     call sa_set_http_header('Content-Type','application/json');
-    Select commandesId, clientId, commandesPrixTOT, commandesQteTOT, commandesDate from tbHistoriques
+    Select commandesId, clientId, commandesPrixTOT, commandesQteTOT, commandesDate from tbCommandes
     where commandesId = commId
 end;
 
  ------------------------------------/* Service pour recevoir l'historique en fonction de l'id de la commande*/---------------------------------------------------
 
-CREATE SERVICE "proc_getHistorique" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.proc_getHistorique(:commid);
+CREATE SERVICE "proc_getCommandes" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.proc_getCommandes(:commid);
