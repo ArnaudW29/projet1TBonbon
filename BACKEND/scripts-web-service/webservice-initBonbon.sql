91c6b13c46fbdd946ea77c3a17fb225d460caa5a
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-08-23 18:42:48 +0200

    Update webservice-initBonbon.sql

--- BACKEND/scripts-web-service/webservice-initBonbon.sql
+++ BACKEND/scripts-web-service/webservice-initBonbon.sql
@@ -1,11 +1,11 @@
 /* Select des info sur les bonbons */
 
 CREATE PROCEDURE "dba"."proc_init_bonbon"()
-RESULT (bonbonId CHAR(3), bonbonNom VARCHAR(45),bonbonGout VARCHAR(15), bonbonType VARCHAR(15), bonbonPrix DECIMAL(4,2),bonbonQte VARCHAR(15),marqueId CHAR(3))
+RESULT (bonbonId CHAR(3), bonbonNom VARCHAR(45), bonbonType VARCHAR(15), bonbonPrix DECIMAL(4,2),bonbonQte VARCHAR(15),marqueId CHAR(3))
 BEGIN 
   call sa_set_http_header('Access-Control-Allow-Origin','*');
   call sa_set_http_header('Content-Type','application/json');
-  select B.bonbonId,B.bonbonNom,B.bonbonGout,B.bonbonType,B.bonbonPrix,B.bonbonQte,M.marqueNom
+  select B.bonbonId,B.bonbonNom,B.bonbonType,B.bonbonPrix,B.bonbonQte,M.marqueNom
   from 
     tbBonbon as B
     inner join tbMarque M
